5d05f47494bb2ec2183123bc223b8b19df246be3
AmirMS90 <amir.m.s1010@gmail.com>
2024-07-14 13:59:09 +0330

    Change the form action

--- src/login.php
+++ src/login.php
@@ -14,7 +14,7 @@
     <a href="./" class="absolute left-2 top-2 rounded-lg rotate-90 hover:cursor-pointer hover:-translate-y-1 hover:opacity-90 transition-all"><img src=".//assets//images//down-arrow.png" height="35" width="35" title="Home Page"></img></a>
     <img class="darkModeBTN absolute right-2 top-2 dark:bg-white bg-slate-400 rounded-lg hover:cursor-pointer hover:-translate-y-1 hover:opacity-90 transition-all" src=".//assets//images//theme_light_dark_icon.png" height="35" width="35"></img>
     <div class="flex h-screen justify-center">
-        <form action="./register.php" method="post" class="flex flex-col self-center">
+        <form action="./login.php" method="post" class="flex flex-col self-center">
             <h1 class="self-center text-3xl font-extrabold m-4 text-green-400">Login</h1>
             <input type="email" name="email" placeholder="Email" class="m-2 rounded-md p-1 w-96 text-black border border-black dark:border-0">
             <input type="password" name="password" placeholder="Password" class="m-2 rounded-md p-1 w-96 text-black border border-black dark:border-0">
